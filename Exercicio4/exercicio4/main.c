6369467d5b95601fe78eb6a8de1337dbbf1aacb5
Renato Tonelli de Souza <renato.souza1969@yahoo.com.br>
2022-09-06 15:38:11 -0300

    aprendendo e executando exerciocios em ligagem C

---
+++ Exercicio4/exercicio4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    system ("title NOME EM ORDEM CRESCENTE THIAGO");
+int i,j,aux,cont=0;
+char vetor[10][10] = {"Andre","Maria","Marcio","Bruno","Henrique","Zoraide","Daniel","Danilo","Zuleica","Adriano"};
+
+
+
+
+for (i=0;i<10;i++)
+printf("%s\n",vetor[i]);
+
+
+return 0;
+}
---
+++ lendoTeclado/lendoTeclado/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int tipoCliente = 0;
+    printf(" Por favor, informe o tipo de cliente
+            digite um numero: \n");
+    scanf( "%i", &num1);
+    printf( "O valor digitado e : %i\n", num1);
+
+    return 0;
+}
